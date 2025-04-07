bcd8c2d7e0fb9a1ef58d9dac64e5ffca6b788cf6
Café <rammoscafe@gmail.com>
2025-04-06 22:06:39 -0300

    yey

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,23 @@
-def fibonacci(x=0, y=1, repeat=100):
-    if repeat <= 0:
-        return y
-    return fibonacci(y, x+y, repeat-1)
-
+def fibonacci(x, dic):
+    if x in dic:
+        return dic[x]
+    else:
+        answer =  fibonacci(x-1,dic) + fibonacci(x-2, dic)
+        dic[x] = answer
+        return answer
 def fib(x):
     if x == 0 or x == 1:
         return 1
     else:
         return fib(x-1) + fib(x-2)
 
-x = fib(12)       
-x2 = fibonacci(0,1,12)
+x = fib(30)       
+dic = {1:1, 2:2}
+x2 = fibonacci(30,dic)
 print(f"This is the final X: {x}")
 print(f"This is the final X2: {x2}")
 
-#both do the same, duh
+#both do the same, 
+#but fibonacci uses a dictionary to dont 
+#waste memory with calculus
+#we did before
