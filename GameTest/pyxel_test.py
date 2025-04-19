e7ecba839ad71979bab5b5d9ef7bf03b3ec2f8d0
Ramos <rammoscafe@gmail.com>
2025-04-18 21:03:57 -0300

    Update pyxel_test.py

--- GameTest/pyxel_test.py
+++ GameTest/pyxel_test.py
@@ -5,6 +5,7 @@ SCREEN_HEIGHT = 120
 
 class App:
     def __init__(self):
+        self.player_x = SCREEN_WIDTH // 2
         #here we define the screen size and title, also the atributes and methods we will use
         pyxel.init(SCREEN_WIDTH, SCREEN_HEIGHT, title="SapooGame") 
         pyxel.mouse(True)
@@ -13,11 +14,17 @@ class App:
     def update(self):
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
+        
+        if pyxel.btn(pyxel.KEY_RIGHT) and self.player_x < SCREEN_WIDTH-15:
+            self.player_x += 1
+        elif pyxel.btn(pyxel.KEY_LEFT) and self.player_x > -1:
+            self.player_x -= 1
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_DARK_BLUE)
         pyxel.blt(SCREEN_WIDTH // 2, 0, 0, 8, 0, 8, 8, pyxel.COLOR_BLACK)
-        pyxel.blt(SCREEN_WIDTH // 2, SCREEN_HEIGHT*4 // 5, 0, 16, 0, 16, 16, pyxel.COLOR_BLACK)
+        pyxel.blt(self.player_x, SCREEN_HEIGHT*4 // 5, 0, 16, 0, 16, 16, pyxel.COLOR_BLACK)
+        pyxel.text(SCREEN_WIDTH // 2, 20, f"{self.player_x}", pyxel.COLOR_WHITE)
 
 App()
  
